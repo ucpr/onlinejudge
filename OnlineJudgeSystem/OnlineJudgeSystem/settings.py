fba0ec3ad7540a769425dff8d01abee73089b84d
ucpr <hoge.uchihara@gmail.com>
2019-04-04 04:20:10 +0900

    feat: login/logoutできるようにした

--- OnlineJudgeSystem/OnlineJudgeSystem/settings.py
+++ OnlineJudgeSystem/OnlineJudgeSystem/settings.py
@@ -54,7 +54,10 @@ ROOT_URLCONF = 'OnlineJudgeSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'accounts', 'templates'),
+#            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
---
+++ OnlineJudgeSystem/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
