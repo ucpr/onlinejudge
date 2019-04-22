dab9f404f0049a29b2f5bc9daedf3205a4da1c69
ucpr <hoge.uchihara@gmail.com>
2019-04-22 23:16:27 +0900

    feat: userのmodelを作った

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,47 +1,75 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, _user_has_perm
+from django.core import validators
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
 
 # Create your models here.
-class AuthUserManager(BaseUserManager):
+class AccountManager(BaseUserManager):
+    def create_user(self, request_data, **kwargs):
+        now = timezone.now()
+        if not request_data['email']:
+            raise ValueError('Users must have an email address')
 
-    def create_user(self, username, email, password):
-        """ Create user """
-        if not username or not email or not password:
-            raise ValueError("Users must have an (username | email | password)")
+        user = self.model(
+            username = request_data['username'],
+            email = self.normalize_email(request_data['email']),
+            is_active = True,
+            last_login = now,
+            date_joined = now,
+        )
 
-        user = self.model(username = username,
-                          email = email,
-                          password = password)
+        user.set_password(request_data['password'])
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        request_data = {
+            'username': username,
+            'email': email,
+            'password': password
+        }
+        user = self.create_user(request_data)
         user.is_active = True
-        user.set_password(password)
-        user.save()
+        user.is_staff = True
+        user.is_admin = True
+        # user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
-class AuthUser(AbstractBaseUser, PermissionsMixin):
-    """
-    ユーザ情報を管理する
-    """
+class Account(AbstractBaseUser):
+    username    = models.CharField(_('username'), max_length=30, unique=True)
+    first_name  = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name   = models.CharField(_('last name'), max_length=30, blank=True)
+    email       = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    is_active   = models.BooleanField(default=True)
+    is_staff    = models.BooleanField(default=False)
+    is_admin    = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def user_has_perm(user, perm, obj):
+        return _user_has_perm(user, perm, obj)
+
+    def has_perm(self, perm, obj=None):
+        return _user_has_perm(self, perm, obj=obj)
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    def get_short_name(self):
+        return self.first_name
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
     class Meta:
-        verbose_name = 'ユーザ'
-        verbose_name_plural = 'ユーザ'
-
-    username = models.CharField(verbose_name='ユーザID',
-                                unique=True,
-                                max_length=30)
-    email = models.EmailField(verbose_name='メールアドレス',
-                              null=True,
-                              default=None)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(verbose_name='有効フラグ',
-                                    default=True)
-    is_staff = models.BooleanField(verbose_name='管理サイトアクセス権限',
-                                   default=False)
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'last_name', 'first_name']
-    objects = AuthUserManager()
-
-    def __str__(self):
-        return self.last_name + ' ' + self.first_name
-
+        db_table = 'api_user'
+        swappable = 'AUTH_USER_MODEL'
