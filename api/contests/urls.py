48cb190ad0bf564972bf9ac78d7f27e8868a0f76
ucpr <hoge.uchihara@gmail.com>
2019-05-02 01:30:28 +0900

    feat: add urls

--- api/contests/urls.py
+++ api/contests/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework import routers
 
-from .views import ContestsView, ContestView
+from .views import ContestsView, ContestView, ProblemsView, ProblemView
 
 
 urlpatterns = [
-    url('contests/', ContestsView.as_view()),
-    url('contests/<str:contest_tag>', ContestView.as_view()),
+    path('contests/', ContestsView.as_view()),
+    path('contests/<str:contest_tag>/', ContestView.as_view()),
+    path('contests/<str:contest_tag>/problems/', ProblemsView.as_view()),
+    path('contests/<str:contest_tag>/problems/<int:id>/', ProblemView.as_view()),
 ]
