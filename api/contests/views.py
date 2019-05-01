203a99f8ab50b1e32b0016337cc8760b34c5343a
ucpr <hoge.uchihara@gmail.com>
2019-05-01 16:34:53 +0900

    feat: GETとPOSTのviewを追加した

--- api/contests/views.py
+++ api/contests/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.core import serializers
+from django.core.paginator import Paginator
+from django.db import transaction
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from rest_framework import authentication, permissions, generics
+from rest_framework_jwt.settings import api_settings
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework import status, viewsets, filters
+from rest_framework.views import APIView
+
+from .serializers import ProblemSerializer, ContestSerializer
+from .models import Contest, Problem
+
+
+class ContestsView(generics.ListAPIView, generics.CreateAPIView):
+    queryset = Contest.objects.all()
+    serializer_class = ContestSerializer
+
+
+#class AuthRegister(generics.CreateAPIView):
+#    permission_classes = (permissions.AllowAny,)
+#    queryset = Account.objects.all()
+#    serializer_class = AccountSerializer
+#
+#    @transaction.atomic
+#    def post(self, request, format=None):
+#        serializer = AccountSerializer(data=request.data)
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
