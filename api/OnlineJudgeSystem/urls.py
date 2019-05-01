4a3166f2da2eabebe239610e53ad05b66f27ce85
ucpr <hoge.uchihara@gmail.com>
2019-05-01 16:31:59 +0900

    feat: add urls

--- api/OnlineJudgeSystem/urls.py
+++ api/OnlineJudgeSystem/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/1.0/', include(api_urlpatterns)),
     path('api/1.0/', include('accounts.urls')),
+    path('api/1.0/', include('contests.urls')),
 ]
