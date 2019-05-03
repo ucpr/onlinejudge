c26f4adec52832bc76caf1301b508cfb7f89da81
ucpr <hoge.uchihara@gmail.com>
2019-05-04 03:41:39 +0900

    feat: submitのシリアライザを追加した

--- api/contests/serializers.py
+++ api/contests/serializers.py
@@ -1,10 +1,10 @@
 from .models import Contest
 from .models import Problem
+from .models import Submittion
 from rest_framework import serializers
 
 
 class ProblemSerializer(serializers.ModelSerializer):
-    # contest_tag = serializers.CharField(required=True)
 
     class Meta:
         model = Problem
@@ -23,4 +23,16 @@ class ContestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Contest
+        fields = '__all__'
+
+
+class SubmittionsSerializer(serializers.ModelSerializer):
+    """ 問題の提出 """
+
+    def create(self, validate_data):
+        # TODO: validate_dataから初期で必要ないやつはnullで初期化する
+        return Submittion(**validate_data)
+
+    class Meta:
+        model = Submittion
         fields = '__all__'
