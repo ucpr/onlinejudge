2dce612f5ea0cf95477bd0014bdccfa1c701690c
ucpr <hoge.uchihara@gmail.com>
2019-05-04 03:41:59 +0900

    feat: routeを追加した

--- api/contests/urls.py
+++ api/contests/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import ContestsView, ContestView, ProblemsView, ProblemView
+from .views import (
+    ContestsView,
+    ContestView,
+    ProblemsView,
+    ProblemView,
+    SubmitView,
+)
 
 
 urlpatterns = [
     path('contests/', ContestsView.as_view()),
     path('contests/<str:contest_tag>/', ContestView.as_view()),
     path('contests/<str:contest_tag>/problems/', ProblemsView.as_view()),
-    path('contests/<str:contest_tag>/problems/<int:id>/', ProblemView.as_view()),
+    path('contests/<str:contest_tag>/problems/<str:problem_tag>/', ProblemView.as_view()),
+    path('contests/<str:contest_tag>/submit/', SubmitView.as_view()),
 ]
