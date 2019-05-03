c2957c4430f5bbdb28f6285ec060860ed9020522
ucpr <hoge.uchihara@gmail.com>
2019-05-04 02:22:21 +0900

    feat: リレーションを追加するためにmodelも順番を入れ替えた

--- api/contests/models.py
+++ api/contests/models.py
@@ -3,29 +3,6 @@ from django.utils.timezone import now
 
 
 # Create your models here.
-class Problem(models.Model):
-    """
-    contest_tag そのコンテストの問題のtag
-    problem_name 問題の名前
-    time_limit 時間制限 (sec)
-    memory_limit メモリ制限 (MB)
-    problem 問題の本文
-    _input 入力
-    output 出力
-    example_input_output 入出力例
-    """
-    contest_tag = models.CharField(max_length=15)
-    problem_name = models.CharField(max_length=20)
-    time_limit = models.IntegerField()
-    memory_limit = models.IntegerField(default=256)
-    problem = models.TextField()
-    input = models.TextField()
-    output = models.TextField()
-    example_input_output = models.TextField()
-    answer = models.BinaryField()
-    foriegn_key = models.ForeignKey(Contest, on_delete=models.CASCADE)
-
-
 class Contest(models.Model):
     """
     is_open コンテストを表示するかしないか
@@ -47,3 +24,26 @@ class Contest(models.Model):
     contest_time = models.IntegerField()
     writer = models.CharField(max_length=30)
     top_page = models.TextField()
+
+
+class Problem(models.Model):
+    """
+    contest_tag そのコンテストの問題のtag
+    problem_name 問題の名前
+    time_limit 時間制限 (sec)
+    memory_limit メモリ制限 (MB)
+    problem 問題の本文
+    _input 入力
+    output 出力
+    example_input_output 入出力例
+    """
+    contest_tag = models.CharField(max_length=15)
+    problem_name = models.CharField(max_length=20)
+    time_limit = models.IntegerField()
+    memory_limit = models.IntegerField(default=256)
+    problem = models.TextField()
+    input = models.TextField()
+    output = models.TextField()
+    example_input_output = models.TextField()
+    answer = models.BinaryField()
+    foriegn_key = models.ForeignKey(Contest, on_delete=models.CASCADE)
