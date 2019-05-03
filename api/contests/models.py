cb50c41be307aee973174bf7555dc562c29361d5
ucpr <hoge.uchihara@gmail.com>
2019-05-04 03:41:24 +0900

    feat: modelを追加した

--- api/contests/models.py
+++ api/contests/models.py
@@ -30,6 +30,9 @@ class Problem(models.Model):
     """
     contest_tag そのコンテストの問題のtag
     problem_name 問題の名前
+    problem_tag 問題のタグ
+    problem_tag 問題の順番
+    problem_order 問題の順番
     time_limit 時間制限 (sec)
     memory_limit メモリ制限 (MB)
     problem 問題の本文
@@ -39,6 +42,8 @@ class Problem(models.Model):
     """
     contest_tag = models.CharField(max_length=15)
     problem_name = models.CharField(max_length=20)
+    problem_tag = models.CharField(max_length=15)
+    problem_order = models.IntegerField()
     time_limit = models.IntegerField()
     memory_limit = models.IntegerField(default=256)
     problem = models.TextField()
@@ -47,3 +52,34 @@ class Problem(models.Model):
     example_input_output = models.TextField()
     answer = models.BinaryField()
     foriegn_key = models.ForeignKey(Contest, on_delete=models.CASCADE)
+
+
+class Submittion(models.Model):
+    """
+    problem_tag 問題のタグ
+    source_code 提出されたソースコード
+    status judgeの結果
+    warning コンパイル時のwarning
+    error エラー
+
+    author 提出したユーザー
+    language 言語
+    time 実行時間
+    memory メモリ使用量
+    byte ソースコードのサイズ
+    date 提出した日付
+    """
+    problem_tag = models.CharField(max_length=15)
+    source_code = models.TextField()
+    status = models.CharField(max_length=5)
+    warning = models.TextField()
+    error = models.TextField()
+
+    author = models.CharField(max_length=10)
+    language = models.CharField(max_length=10)
+    time = models.FloatField()
+    memory = models.IntegerField()
+    byte = models.IntegerField()
+    date = models.DateTimeField()
+
+    foriegn_key = models.ForeignKey(Problem, on_delete=models.CASCADE)
