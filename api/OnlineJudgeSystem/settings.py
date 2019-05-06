97dea736a629c17a673ef5da6296817a0f40c463
ucpr <hoge.uchihara@gmail.com>
2019-05-06 21:17:32 +0900

    feat: DATABASEをpostgresにした

--- api/OnlineJudgeSystem/settings.py
+++ api/OnlineJudgeSystem/settings.py
@@ -138,10 +138,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'daizuhogehoge',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
