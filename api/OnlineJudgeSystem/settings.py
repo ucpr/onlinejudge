ad94055c69eaae4b7614ffbebc123e8ee1b9af80
ucpr <hoge.uchihara@gmail.com>
2019-05-06 19:37:26 +0900

    feat: add settings

--- api/OnlineJudgeSystem/settings.py
+++ api/OnlineJudgeSystem/settings.py
@@ -125,10 +125,13 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
