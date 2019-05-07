1cf02d89816ceb0789c61cd4bd0890038182a1f5
nemusou <ryubi.102395@gmail.com>
2019-05-08 03:14:02 +0900

    fix: リストで囲み忘れていたのを直した

--- judge_server/src/submit_data.py
+++ judge_server/src/submit_data.py
@@ -2,9 +2,10 @@ from collections import namedtuple
 
 
 SubmitData = namedtuple('SubmitData',
-                        'submission_id',
-                        'contest_tag',
-                        'problem_tag',
-                        'source_code',
-                        'language'
-                        )
+                        [
+                            'submission_id',
+                            'contest_tag',
+                            'problem_tag',
+                            'source_code',
+                            'language'
+                        ])
